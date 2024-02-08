0345c6c680aadbd2f4738a71ae4ce84ca5961af8
gedeonagmas <gedeonagmas2580@gmail.com>
2024-02-08 15:12:59 +0300

    MONGOURI

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
-
+const MONGO_DB_URI =
+  "mongodb+srv://user:z1OoheclB06GxVER@cluster0.rhayeki.mongodb.net/CHAT?retryWrites=true&w=majority";
 const connectToMongoDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
-	}
+  try {
+    await mongoose.connect(MONGO_DB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error.message);
+  }
 };
 
 export default connectToMongoDB;
