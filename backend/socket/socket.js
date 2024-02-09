f44a49cfe4448a93a243eaf978b1f287350e5568
gedeonagmas <gedeonagmas2580@gmail.com>
2024-02-09 10:39:33 +0300

    hhhh

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -5,16 +5,19 @@ import express from "express";
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    // origin: ["https://test-chat-1-client.vercel.app"],
-    origin: "*",
-    methods: ["POST", "GET", "PATCH", "DELETE", "PUT"],
-    credentials: true,
-  },
-});
+const io = new Server(
+  server
+  // {
+  // cors: {
+  //   // origin: ["https://test-chat-1-client.vercel.app"],
+  //   origin: "*",
+  //   methods: ["POST", "GET", "PATCH", "DELETE", "PUT"],
+  //   credentials: true,
+  // },
+  // }
+);
 
-// io.origins('*:*');
+io.origins("*:*");
 export const getReceiverSocketId = (receiverId) => {
   return userSocketMap[receiverId];
 };
